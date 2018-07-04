5bdc5de4ab902940c8ea636d165856127ff2bcda
tantowibudi23 <tantowibudi23@gmail.com>
2018-07-04 15:59:23 +0700

    a

--- index.php
+++ index.php
@@ -96,6 +96,7 @@ switch (ENVIRONMENT)
  *
  * This variable must contain the name of your "system" directory.
  * Set the path if it is not in the same directory as this file.
+ *---------------------------------------------------------------
  */
 	$system_path = 'system';
 
